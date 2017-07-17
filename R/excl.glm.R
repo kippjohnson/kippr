c2d3ee00b005cddce45b764aaacecf1b0e400e01
Kipp Johnson <kipp.william.johnson@gmail.com>
2017-07-17 11:18:13 -0400

    Typo fixes to excl.glm

--- R/excl.glm.R
+++ R/excl.glm.R
@@ -1,12 +1,12 @@
-#' Easily exclude name columns in glm()
+#' Easily exclude columns in glm()
 #'
 #' Preserve the " Y ~ . " formula in a glm, but exclude specific named columns
 #' from the "." (i.e., exclude a column or columns from the rest of the
 #' variables in the dataset).
 #'
-#' @param The Y variable
-#' @param indata The dataset
-#' @param excl.cols Vector of column names or column numbers to include
+#' @param Y Dependent variable
+#' @param indata Dataset
+#' @param excl.cols Vector of column names or vector of column numbers to exclude
 #' @export
 #' @examples
 #'
