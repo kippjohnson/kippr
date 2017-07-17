65366578fac68324193e1f192273dfb1d497ecf7
Kipp Johnson <kipp.william.johnson@gmail.com>
2017-07-17 14:07:17 -0400

    excl.glm now works with additional args

--- R/excl.glm.R
+++ R/excl.glm.R
@@ -9,23 +9,37 @@
 #' @param excl.cols Vector of column names or vector of column numbers to exclude
 #' @export
 #' @examples
-#'
+#' data(infert)
 #' m1 <- excl.glm("conc", Theoph, excl.cols="Subject")
 #' summary(m1)
 #'
-#' m1 <- excl.glm("conc", Theoph, excl.cols=c(1,2))
-#' summary(m1)
+#' m2 <- excl.glm("conc", Theoph, excl.cols=c(1,2))
+#' summary(m2)
+#'
+#' m3 <- excl.glm("case", infert, excl.cols="Subject", family=binomial())
+#' summary(m3)
+
 
 
 excl.glm <- function(depvar, indata, excl.cols=NULL, ...){
     form <- as.formula(paste0(depvar, " ~ ."))
 
     if(class(excl.cols)=="character"){
-        mod <- glm(form,
-               data=indata[, !(colnames(indata) %in% excl.cols)])
+
+        data=indata[, !(colnames(indata) %in% excl.cols)]
+
         }else if(class(excl.cols)=="numeric"){
-        mod <- glm(form,
-                   data=indata[, -excl.cols])
-        }
+
+        data=indata[, -excl.cols]
+                }
+
+    args <- list(formula = form,
+                 data = indata,
+                 ... )
+
+    mod <- do.call(glm, args)
+
     return(mod)
 }
+
+
